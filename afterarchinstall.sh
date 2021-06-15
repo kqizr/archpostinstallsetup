b48d628e391ebbc0070ab0f8350f95305dca7423
sneccolol2k <85910739+sneccolol2k@users.noreply.github.com>
2021-06-15 01:39:36 +0000

    Update afterarchinstall.sh

--- afterarchinstall.sh
+++ afterarchinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Installs Essential Packages I will need
-yay -S plasma ark dolphin konsole ark papirus-icon-theme librewolf-bin latte-dock mpv qbittorrent
+sudo pacman -S plasma ark dolphin konsole ark papirus-icon-theme librewolf-bin latte-dock mpv qbittorrent
 sudo systemctl enable sddm
 
 # Installs necessary fonts.
-yay -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family ttf-ms-fonts
+sudo pacman -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family ttf-ms-fonts
