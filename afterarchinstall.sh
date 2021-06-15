c9f30e74d1443ebda087f385a40215eb83e40204
sneccolol2k <85910739+sneccolol2k@users.noreply.github.com>
2021-06-15 00:42:21 +0000

    Add files via upload

---
+++ afterarchinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#Installs Yay, The AUR Helper.
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ..
+
+# Installs Essential Packages I will need
+yay -S plasma ark dolphin konsole ark papirus-icon-theme librewolf-bin latte-dock mpv 
+sudo systemctl enable sddm
+
+# Installs necessary fonts.
+yay -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family ttf-ms-fonts
