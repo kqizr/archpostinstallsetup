b08371533f27208799f371af09719c7a187c7697
Joumo <85910739+Joumo@users.noreply.github.com>
2021-07-30 18:03:37 -0700

    Update afterarchinstall.sh

--- afterarchinstall.sh
+++ afterarchinstall.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
 # Installs Essential Packages I will need
-sudo pacman -S plasma ark dolphin konsole gwenview qt5-imageformats ark papirus-icon-theme firefox latte-dock mpv qbittorrent
+sudo pacman -S i3-gaps rofi picom cmus qutebrowser lightdm lightdm-gtk-greeter
 
 # Start The Display Manager
-sudo systemctl enable sddm
+sudo systemctl enable lightdm
 
 # Installs necessary fonts.
 sudo pacman -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family
-
-# Install yay
-cd 
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
