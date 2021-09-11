8ea855266a540ec2d50a135ad1b08fd544df7f37
Joumo <85910739+Joumo@users.noreply.github.com>
2021-09-10 23:04:43 -0700

    Update afterarchinstall.sh

--- afterarchinstall.sh
+++ afterarchinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Installs Essential Packages I will need
-sudo pacman -S i3-gaps rofi picom cmus qutebrowser lightdm lightdm-gtk-greeter alacritty ranger
+sudo pacman -S i3-gaps xorg xorg-xinit xord-xrdb rofi picom cmus chromium alacritty ranger
 
-# Start The Display Manager
-sudo systemctl enable lightdm
+# Creates the Xinitrc File
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 # Installs necessary fonts.
 sudo pacman -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family
