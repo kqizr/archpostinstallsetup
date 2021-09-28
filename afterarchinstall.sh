de9176dd576136c569884d9c17c93ff169eadcbc
Joumo <85910739+Joumo@users.noreply.github.com>
2021-09-28 12:30:11 -0700

    Update afterarchinstall.sh

--- afterarchinstall.sh
+++ afterarchinstall.sh
@@ -3,8 +3,8 @@
 # Installs Essential Packages I will need
 sudo pacman -S i3-gaps xorg xorg-xinit xord-xrdb rofi picom cmus chromium alacritty ranger
 
-# Creates the Xinitrc File
-sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
-
 # Installs necessary fonts.
 sudo pacman -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family
+
+# Creates the Xinitrc File
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
