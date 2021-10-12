a4a7a68e9b352ca1d6fc04ef55e863b3f5943b30
Kuraisenn <85910739+Kuraisenn@users.noreply.github.com>
2021-10-11 19:13:45 -0700

    Update afterarchinstall.sh

--- afterarchinstall.sh
+++ afterarchinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Installs Essential Packages I will need
-sudo pacman -S i3-gaps xorg xorg-xinit xord-xrdb rofi picom cmus chromium alacritty ranger
+sudo pacman -S i3-gaps xorg-xinit xorg-xrdb rofi picom cmus chromium alacritty ranger feh
 
 # Installs necessary fonts.
 sudo pacman -S ttf-liberation ttf-dejavu adobe-source-sans-pro-fonts ttf-droid ttf-bitstream-vera ttf-anonymous-pro ttf-ubuntu-font-family
